f7b41e382b97b6a3248c356ce60b993a8073ee1f
RobertoFloris <florisroberto99@gmail.com>
2024-11-20 19:58:42 +0100

    [fix] snack 2

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -5,9 +5,27 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+// function onlyLetter(array, letter){
+//   const newArray = [];
+//   for (let i=0; i<array.length; i++){
+//     if(array[i].at(0).toLowerCase() === letter.toLowerCase())
+//       newArray.push(array[i])
+//   }
+//   return newArray;
+// }
+
+const onlyLetter = (array, letter) => {
+  const newArray = [];
+  for (let i=0; i<array.length; i++){
+    if(array[i].at(0).toLowerCase() === letter.toLowerCase())
+      newArray.push(array[i])
+  }
+  return newArray;
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(onlyLetter(names, "A"))
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
