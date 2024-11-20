4d6b969b5313e878650007d9ca4e38abe6c8f5e0
RobertoFloris <florisroberto99@gmail.com>
2024-11-20 20:18:36 +0100

    [fix] snack 3

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -1,13 +1,25 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
+const word = 'jAvAscript';
 
 
 // Dichiara la funzione qui.
+function vowelCount (string){
+  let count=0;
+  const vowel = ["a","e","i","o","u"];
+  for (let i=0; i<string.length; i++){
+    for (let j=0; j<vowel.length; j++){
+      if(string.at(i).toLowerCase() === vowel[j]){
+        count++;
+      }
+    }
+  }
+  return count;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(vowelCount(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
